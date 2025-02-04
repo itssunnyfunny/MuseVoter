081c2830e5dc53d76b83269d4957891c9878084b
itssunnyfunny <sunnyfunny4062@gmail.com>
2025-02-04 14:45:19 +0530

    adding providers to the layout file

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -29,7 +29,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Providers>
+
         {children}
+        
         </Providers>
       </body>
     </html>
