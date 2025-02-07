1366230a3e2262cae9cf6d1d3f7a8b1c5373eaed
itssunnyfunny <sunnyfunny4062@gmail.com>
2025-02-07 12:19:44 +0530

    adding a package for title and thumbnail

--- app/app/api/streams/route.ts
+++ app/app/api/streams/route.ts
@@ -1,6 +1,8 @@
 import { prismaClient } from '@/app/lib/db';
 import { NextRequest, NextResponse } from 'next/server';
 import { Stream } from 'stream';
+//@ts-ignore
+import youtubesearchapi from 'youtube-search-api'
 import {z} from 'zod'
 const YT_REGEX: RegExp = /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:(?:watch\?(?:.*&)?v=)|embed\/)|youtu\.be\/)([A-Za-z0-9_-]{11})(?:[?&]\S*)?$/;
 
@@ -24,15 +26,25 @@ export async function POST(req:NextRequest) {
         }
 
         const extractedId = data.url.split("?v=")[1];
+
+        const res = await youtubesearchapi.GetVideoDetails(extractedId);
+         const thumbnails = res.thumbnail.thumbnails;
+
+
+         thumbnails.sort((a : {width: number},b: {width: number})=> a.width < a.width ? -1 : 1)
+
       const stream =   await prismaClient.stream.create({
           data: {
             userId: data.creatorId,
             url: data.url,
             extractedId,
-            type: "youtube"
+            type: "youtube",
+            title: res.tilte ?? "Can't find video",
+            smallImg: thumbnails[thumbnails.length -1].url ?? "https://static.vecteezy.com/system/resources/thumbnails/008/568/878/small/website-page-not-found-error-404-oops-worried-robot-character-peeking-out-of-outer-space-site-crash-on-technical-work-web-design-template-with-chatbot-mascot-cartoon-online-bot-assistance-failure-vector.jpg",
+            bigImg: thumbnails.length > 1 ? thumbnails[thumbnails.length -2].url : thumbnails[thumbnails.length - 1].url ?? "https://static.vecteezy.com/system/resources/thumbnails/008/568/878/small/website-page-not-found-error-404-oops-worried-robot-character-peeking-out-of-outer-space-site-crash-on-technical-work-web-design-template-with-chatbot-mascot-cartoon-online-bot-assistance-failure-vector.jpg"
           }
         })
-        NextResponse.json({
+      return  NextResponse.json({
             message: "Stream is Added",
             id: stream.id
 
